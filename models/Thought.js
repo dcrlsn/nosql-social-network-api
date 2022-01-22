76e2dec67090552188efbbc786874e7dc5afe4d2
dcrlsn <c4r150n@gmail.com>
2022-01-21 20:03:34 -0500

    initial commit

---
+++ models/Thought.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ThoughtSchema = new Schema(
+
+)
+
+
+const Thought = model('thought', ThoughtSchema)
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+
+)
+
+
+const User = model('user', UserSchema)
+module.exports = User;
